fc07dffd84dbeb1713e54e8a2d22925e99098421
vipulob <vipulcell@gmail.com>
2015-05-11 22:14:20 +0530

    Created Login Hyperlink and Login Page

--- quotes/urls.py
+++ quotes/urls.py
@@ -8,4 +8,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$',views.index,name='index'),
+    url(r'^login$', views.login, name='login'),
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -9,11 +9,23 @@ from . import forms
 from models import Quote
 
 
+# First Page
 def index(request):
 
-    login
     # Get the quotes from the database
     quote_list = Quote.objects.values_list('quote_text', flat=True)
     form = forms.QuoteForm()
-    return render(request,"practice/index.html",{"form":form,
+    return render(request,"quotes/index.html",{"form":form,
                                                  "quote_list":quote_list})
+
+
+# Displays login page
+def login(request):
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request.POST)
+        print("In POST1")
+        return HttpResponseRedirect('/thanks/')
+
+    form = AuthenticationForm()
+    return render(request,"quotes/login.html",{'form':form})
